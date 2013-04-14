3d73b7359d8ec7fcdc3eddded98c37e1b29a3baf
Vince Speelman <v@vinspee.me>
2013-04-13 21:43:58 -0400

    livereload scripts and templates

--- Gruntfile.js
+++ Gruntfile.js
@@ -41,7 +41,7 @@ module.exports = function (grunt) {
 				files: [
 					'<%= yeoman.app %>/*.html',
 					'{.tmp,<%= yeoman.app %>}/styles/{,*/}*.css',
-					'{.tmp,<%= yeoman.app %>}/scripts/{,*/}*.js',
+					'{.tmp,<%= yeoman.app %>}/scripts/**/*.js',
 					'<%= yeoman.app %>/images/{,*/}*.{png,jpg,jpeg,webp}'
 				],
 				tasks: ['livereload']
